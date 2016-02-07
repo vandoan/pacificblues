8495a0f11f8baf3100f8c3d0264b716d4012defb
Van Doan <vdoan09@gmail.com>
2016-02-07 09:30:12 -0500

    initiating project whale, a tailored site

---
+++ homepage.php
@@ -0,0 +1,33 @@
+<?php /* Template Name: HomePage */ ?>
+
+<style>
+body { background-color: #ECECEC; }
+#pac-header { 
+	width:100%; 
+	float:left; 
+    padding: 40px 0;
+	text-align: center;
+    -webkit-font-smoothing: antialiased;
+	}
+#pac-header > div a { 
+    color: #2B2B2B;
+    font-size: 24px;
+    letter-spacing: 6px;
+   	text-decoration: none;
+   	font-family: Montserrat, "Helvetica Neue", sans-serif;
+}
+</style>
+
+<div id='pac-header'>
+	<div><a href="http://pacificblues.co">pacific blues</a></div>
+</div>
+
+
+
+<img src="http://pacificblues.co/wp-content/uploads/2016/02/cropped-diver-turtle.png" 
+srcset="http://pacificblues.co/wp-content/uploads/2016/02/cropped-diver-turtle-300x118.png 300w, http://pacificblues.co/wp-content/uploads/2016/02/cropped-diver-turtle-768x301.png 768w, http://pacificblues.co/wp-content/uploads/2016/02/cropped-diver-turtle-1024x402.png 1024w, http://pacificblues.co/wp-content/uploads/2016/02/cropped-diver-turtle.png 1200w" sizes="(max-width: 709px) 85vw, (max-width: 909px) 81vw, 
+(max-width: 1362px) 88vw, 
+1200px" width="1200" height="471" alt="Pacific Blues">
+
+
+
