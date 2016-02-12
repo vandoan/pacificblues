3421d89758b48e3bef3d803dbe471682e8505ebe
Van Doan <vdoan09@gmail.com>
2016-02-12 06:24:32 -0500

    Updated home page and building out first gallery page

---
+++ kelpforest.php
@@ -0,0 +1,69 @@
+<?php /* Template Name: KelpGalleryPage */ ?>
+
+<style>
+
+#header-section {
+	width: 100%;
+	height: 50px;
+	background-color: lavender;
+	text-align: center;
+}
+#kelp-main {}
+#hero-section {
+	width: 100%;
+	height: 500px;
+	background-color: blue;
+	position: relative;
+	float:left;
+}
+#hero-image {
+	background-color: #FFFFFF;
+	width: 600px;
+	height: 500px;
+	position: relative;
+	display: block;
+	margin: auto;
+}
+#hero-image div {
+	height: 200px;
+	width: 400px;
+	background-color: green;
+	margin: auto;
+    bottom: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+#slide-show {
+	width: 100%;
+	background-color: yellow;
+	height:	300px;
+
+}
+
+</style>
+
+
+
+<div id='header-section'>
+	<div id='header-text'>Pacific Blue</div>
+</div>
+
+<div id='kelp-main'>
+	<div id='hero-section'></div>
+		<div id='hero-image'><div><img src=''/></div></div>
+	</div>
+
+	<div id='slide-show'>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+		<div class='slide'> </div>
+	</div>
+</div>
---
+++ notes.md
@@ -0,0 +1,17 @@
+
+
+color palette
+ - http://www.flatuicolorpicker.com/
+
+css animation
+ - http://www.tutorialspoint.com/css/css_animation.htm
+ - http://www.tutorialspoint.com/css/css_animation_fade_in.htm
+
+ word press theme cheat sheet
+ - http://www.wpbeginner.com/wp-themes/wordpress-theme-cheat-sheet-for-beginners/
+
+ fading in and out and again
+ - http://stackoverflow.com/questions/30125705/css-how-make-an-element-fade-in-and-then-fade-out
+
+ 
+
